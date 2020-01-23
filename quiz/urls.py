aa6ed40b498dab71e3a8d888ffd0a167f3141181
duplxey <nick.tomazic@gmail.com>
2020-01-23 19:29:08 +0100

    Search now actually works.

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view, name='quiz-index'),
-    path('search/<str:query>/', views.search_view, name='quiz-index'),
+    path('search/', views.search_view, name='quiz-search'),
     path('learn/<str:document_id>/', views.learn_view, name='quiz-learn'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from x5quiz.errors import search_failed
 from x5quiz.x5gon import search_documents, get_document
 
 
@@ -7,9 +8,15 @@ def index_view(request):
     return render(request, "quiz/index.html", {})
 
 
-def search_view(request, query):
-    keywords = query.replace(" ", "")
+def search_view(request):
+    query = None
+    if request.method == 'POST':
+        query = request.POST['query']
 
+    if query is None or len(query) < 2:
+        return search_failed(request)
+
+    query = query.replace(" ", "")
     return render(request, "quiz/search.html", {'keyword': query, 'results': search_documents(query)})
 
 
