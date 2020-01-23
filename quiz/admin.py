1422a94401f0cf2420b8cf713c6432f05e1e7b9e
duplxey <nick.tomazic@gmail.com>
2020-01-23 21:40:47 +0100

    Created ResourceStatistics model.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import ResourceStatistics
+
+admin.site.register(ResourceStatistics)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ResourceStatistics(models.Model):
+    resource_id = models.IntegerField(unique=True)
+    rating = models.FloatField(default=5)
+    raters = models.ManyToManyField(User)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.resource_id) + "'s statistics"
